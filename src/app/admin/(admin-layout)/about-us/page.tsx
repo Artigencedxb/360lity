741f9fbc5e40aac87dde7e7464096e2d3033622a
Hibathulla <hibathullacm@gmail.com>
2023-11-12 17:31:37 +0530

    refactor(error): error fix

--- src/app/admin/(admin-layout)/about-us/page.tsx
+++ src/app/admin/(admin-layout)/about-us/page.tsx
@@ -2,17 +2,23 @@
 import { useGetAbout } from "../../../../api/about";
 import Header from "@/common/Header";
 import AboutusForm from "@/components/Aboutus/AboutusForm";
+import { useEffect, useState } from "react";
 
 const AboutPage = () => {
+  const [state, setState] = useState(false);
   const { data } = useGetAbout();
 
   const about = data?.data?.about;
 
-  if (about) {
+  useEffect(() => {
+    setState(true);
+  }, []);
+
+  if (about && state) {
     return (
       <div className="pb-10">
         <Header heading="About us" />{" "}
-        {/* <AboutusForm initialValues={data?.data?.about} /> */}
+        <AboutusForm initialValues={data?.data?.about} />
       </div>
     );
   }
--- src/app/admin/(admin-layout)/blog/create/page.tsx
+++ src/app/admin/(admin-layout)/blog/create/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 import Header from "@/common/Header";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BlogAddForm from "@/components/Blog/BlogForm";
 
 const AddBlogsPage = () => {
+  const [state, setState] = useState(false);
+
+  useEffect(() => {
+    setState(true);
+  }, []);
+
+  if(state) {
   return (
     <div className="py-10">
       <Header heading="Add Blog" />
       <BlogAddForm />
     </div>
   );
+}
+return null
 };
 
 export default AddBlogsPage;

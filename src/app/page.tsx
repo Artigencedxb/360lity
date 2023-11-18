11d01a772f4b513c66fe71f8abc27af208b0a144
hibathulla <hibathulla@ipixsolutions.com>
2023-11-18 13:46:27 +0530

    refactor(change)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen pb-10 sm:pb-0">
       <iframe
         src="https://360lity.com/projects/web/"
         name="360lity"
-        scrolling="no"
         width="100%"
         height="100%"
+        // className="h-full"
         allowFullScreen
       ></iframe>
     </div>
